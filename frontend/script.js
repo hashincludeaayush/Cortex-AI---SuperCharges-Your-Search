bbc36fc31868a7313d6d7cd220a5525bac505223
Aayush Singh <202117b3731@wilp.bits-pilani.ac.in>
2023-02-14 02:03:44 +0530

    added production backend http server

--- frontend/script.js
+++ frontend/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('https://cortexa.shoto.cloud', {
+  const response = await fetch('http://localhost:5000', {
     method: 'POST',
     headers:{
       'Content-Type': 'application/json'
