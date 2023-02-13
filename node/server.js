4a989c4cb21552ba4542eb0c5d88ffda5f91c7db
Aayush Singh <202117b3731@wilp.bits-pilani.ac.in>
2023-02-14 01:10:30 +0530

    add express support for backend http server

--- node/server.js
+++ node/server.js
@@ -1,3 +1,4 @@
+const http = require('http');
 const app = require('express')();
 
 const hostname = '127.0.0.1';
@@ -5,10 +6,10 @@ const port = 5000;
 
 app.get("/",(req, res) => {
   res.statusCode = 200;
-  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Content-Type', 'text/plain');
   res.end('hehehe\n');
 });
 
-app.listen(port, hostname, () => {
+app.listen(port, () => {
   console.log(`Server running at http://:/`);
 });
