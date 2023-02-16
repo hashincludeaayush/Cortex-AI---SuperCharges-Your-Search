54d25a24b9e82f9267e9f6f1964c13100fc7e17f
Aayush Singh <202117b3731@wilp.bits-pilani.ac.in>
2023-02-17 03:10:17 +0530

    added new designs in css

--- backend/server.js
+++ backend/server.js
@@ -23,13 +23,13 @@ const openai = new OpenAIApi(configuration);
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get('/', async(req, res) =>{
+app.get('/api', async(req, res) =>{
 res.status(200).send({
     message: 'Hello from Cortex AI'
 })
 })
 
-app.post('/', async(req, res) =>{
+app.post('/api', async(req, res) =>{
 try{
   const prompt = req.body.prompt;
   const response = await openai.createCompletion({
@@ -38,7 +38,7 @@ try{
   temperature: 0,
   max_tokens: 3000,
   top_p: 1,
-  frequency_penalty: 0.5,
+  frequency_penalty: 1,
   presence_penalty: 0,
   });
 
